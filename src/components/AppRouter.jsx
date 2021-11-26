9a073b8b3816d38ce570ba143d3ed1f3074c6c6a
Alexandr-ace <syakubin@bk.ru>
2021-11-27 00:14:28 +0300

    twelve

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { privateRoutes, publicRoutes } from "../router/routes";
+import { useContext } from "react";
+import { AuthContext } from "../context";
+import Loader from "./UI/Loader/Loader";
 
 const AppRouter = () => {
-    const isAuth = true;
+    const { isAuth, isLoading } = useContext(AuthContext);
+    console.log(isAuth);
+    if (isLoading) {
+        return <Loader />;
+    }
     return isAuth ? (
         <Routes>
             {privateRoutes.map((route) => (
@@ -11,6 +18,7 @@ const AppRouter = () => {
                     path={route.path}
                     element={<route.component />}
                     exact={route.exact}
+                    key={route.path}
                 />
             ))}
             <Route path="*" element={<Navigate to="/posts" />} />
@@ -22,6 +30,7 @@ const AppRouter = () => {
                     path={route.path}
                     element={<route.component />}
                     exact={route.exact}
+                    key={route.path}
                 />
             ))}
             <Route path="*" element={<Navigate to="/login" />} />
